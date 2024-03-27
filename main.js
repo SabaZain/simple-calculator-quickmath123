325b96adb45488594db8c2848c81810a6ae1a9a6
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-03-27 13:59:19 +0500

    Adding my simple calculator project

---
+++ main.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+// Simple Calculator Using Inquirer
+import inquirer from "inquirer";
+console.log("Welcome to codewithsaba - simple calculator");
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Kindly enter your first no:"
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Kindly enter your second no:"
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/", "%", "**"],
+        message: "Select Operator:"
+    },
+]);
+const { numberOne, numberTwo, operator } = answers;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    else if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    else if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    else if (operator === "%") {
+        result = numberOne / numberTwo * 100;
+    }
+    else if (operator === "**") {
+        result = numberOne ** numberTwo;
+    }
+    console.log("Your result is : ", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
+console.log("Thankyou for using codewithsaba - simple calculator");
